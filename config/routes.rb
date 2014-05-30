57ba445660e93de6651488476e3882ce03e481af
Alexander <a1exander@inbox.ru>
2014-05-30 23:02:57 +0400

    Добавил и связал модели patient и writing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 MedicalHistory::Application.routes.draw do
+  resources :writings
+
+  resources :patients
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
