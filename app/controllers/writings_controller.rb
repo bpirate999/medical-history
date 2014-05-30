a43e3ed4ffc052b347d8f9a5ab917e98fae13fe7
Alexander <a1exander@inbox.ru>
2014-05-31 01:20:59 +0400

    Мелкие исправления

--- app/controllers/writings_controller.rb
+++ app/controllers/writings_controller.rb
@@ -56,7 +56,10 @@ class WritingsController < ApplicationController
   # DELETE /writings/1
   # DELETE /writings/1.json
   def destroy
+    @patient = Patient.find(params[:patient_id])
+    @writing = @patient.writings.find(params[:id])
     @writing.destroy
+    
     respond_to do |format|
       format.html { redirect_to writings_url }
       format.json { head :no_content }
